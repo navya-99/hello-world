7236b53eb2ccd447338cd5df3de1c7691046ddf8
navya-99 <67654584+navya-99@users.noreply.github.com>
2023-09-30 12:27:30 +0530

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,27 @@
+
+    console.log("Navya Padiyar");
+    console.log("I am from Mangalore");
+    console.log("Javascript");
+    console.log("23 years");
+    
+
+    console.log("3"+3);
+    console.log("3+3");
+    console.log(3+"3");
+    console.log("3"+"3");
+    console.log(3-3+3);
+    console.log(3+"3"+"3");
+    console.log(3,3);
+
+    let num = "20";
+
+   console.log(typeof(num));
+
+   console.log(3+3+"3");
+   console.log("3"+3+3);
+
+   let a=b=10;
+
+   console.log(a);
+   console.log(b);
+
